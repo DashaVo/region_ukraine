03b8e513f858de5d0b077a6f98c18a3fe8680599
DashaVo <69368556+DashaVo@users.noreply.github.com>
2022-05-30 16:08:36 +0300

    test image, path fix

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,12 @@
 import Image from 'next/image';
-import Layout from "/components/Layout";
+import Layout from '/components/Layout';
 
 export default function Custom404() {
     return(
         <Layout>
             <h1>404 - Page Not Found(<br/> Look a meme</h1>
             <Image
-                src="/../public/images/bilgolod.png"
+                src="/public/images/bilgolod.png"
                 height={544}
                 width={544}
                 alt="meme"
