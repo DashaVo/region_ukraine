37220a9a8527808f8e3d3456f30ba23c2305e5aa
DashaVo <69368556+DashaVo@users.noreply.github.com>
2022-05-30 15:56:59 +0300

    test image

--- pages/regions/[id].js
+++ pages/regions/[id].js
@@ -1,4 +1,4 @@
-import Layout, {siteTitle} from '../../components/layout';
+import Layout from '../../components/layout';
 import {ApolloClient, gql, InMemoryCache} from "@apollo/client";
 import Image from 'next/image'
 import Head from "next/head";
