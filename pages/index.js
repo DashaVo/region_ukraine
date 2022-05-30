c98b81f483eecc76179f98ab5f52547ad4eddd6e
DashaVo <69368556+DashaVo@users.noreply.github.com>
2022-05-30 16:15:33 +0300

    register "L"

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '/components/layout';
+import Layout, { siteTitle } from '/components/Layout';
 import utilStyles from '/styles/utils.module.css';
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import Regions from "/components/regions";
